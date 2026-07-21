704b50107aa9781f67a805c3312e149e51db3583
幽 <lingjiedao@outlook.com>
2026-07-22 00:58:00 +0800

    Add files via upload

---
+++ 【开始】批量生成360色相.cmd
@@ -0,0 +1,7 @@
+@echo off
+setlocal enabledelayedexpansion
+
+
+py [360色相]批量生成.py 输出360色相素材组.proto
+
+pause
---
+++ 【第二版】gil存档素材组追加内容/【开始】批量生成gil素材组范围.cmd
@@ -0,0 +1,7 @@
+@echo off
+setlocal enabledelayedexpansion
+
+
+py [gil素材组]批量生成.py 输出的gil360度颜色素材组结构
+
+pause
